2b3f420567db9d0ffbea2fee815f4a131603edd9
PinelopiKopanou <65811416+PinelopiKopanou@users.noreply.github.com>
2024-05-20 18:13:10 +0300

    Add files via upload

--- calories-age.sql
+++ calories-age.sql
@@ -2,7 +2,7 @@
 
 DELIMITER //
 
-CREATE PROCEDURE IF NOT EXISTS calculate_calories_per_portion(IN recipeID INT)
+CREATE PROCEDURE IF NOT EXISTS calculate_calories_per_portion(IN Name VARCHAR(100))
 BEGIN
     DECLARE total_calories INT;
     DECLARE portions INT;
@@ -12,58 +12,88 @@ BEGIN
     INTO total_calories
     FROM Recipe_has_Ingredients ri
     JOIN Ingredients i ON ri.Ingredients_name = i.name
-    WHERE ri.Recipe_name = recipeID;
+    WHERE ri.Recipe_name = Name;
 
     -- receive number of portions
-    SELECT portions
+    SELECT r.portions
     INTO portions
-    FROM Recipe
-    WHERE name = recipeID;
+    FROM Recipe r
+    WHERE r.name = Name;
 
-    -- insert/update "calories_per_portion" in table "Nutrition_Info"
+    
+	
+	-- update "calories_per_portion" in table "Nutrition_Info"
+    UPDATE Nutrition_Info
+    SET calories_per_portion = total_calories / portions
+    WHERE Recipe_name = name;
+	
+	
     INSERT INTO Nutrition_Info (Recipe_name, calories_per_portion)
-    VALUES (recipeID, total_calories / portions)
-    ON DUPLICATE KEY UPDATE calories_per_portion = VALUES(calories_per_portion);
+    VALUES (Name, total_calories / portions)
+	ON DUPLICATE KEY UPDATE calories_per_portion = VALUES(calories_per_portion);
+    
+	
 END //
 
 DELIMITER ;
 
 
+
 -- Triggers
--- for inserts/updates of an ingredient
+
+-- for inserts/updates/deletes of an ingredient
+DELIMITER //
+
 CREATE TRIGGER IF NOT EXISTS update_calories_per_portion_after_insert
 AFTER INSERT ON Recipe_has_Ingredients
 FOR EACH ROW
 BEGIN
-    CALL calculate_calories_per_portion(NEW.Recipe_name)
+    CALL calculate_calories_per_portion(NEW.Recipe_name);
 END //
 
-DELIMITER ;
 
 CREATE TRIGGER IF NOT EXISTS update_calories_per_portion_after_update
 AFTER UPDATE ON Recipe_has_Ingredients
 FOR EACH ROW
 BEGIN
-    CALL calculate_calories_per_portion(NEW.Recipe_name)
+    CALL calculate_calories_per_portion(NEW.Recipe_name);
+END //
+
+
+CREATE TRIGGER IF NOT EXISTS update_calories_per_portion_after_delete
+AFTER DELETE ON Recipe_has_Ingredients
+FOR EACH ROW
+BEGIN
+    CALL calculate_calories_per_portion(OLD.Recipe_name);
 END //
 
 DELIMITER ;
 
--- for inserts/updates of a recipe
+
+-- for inserts/updates/deletes of a recipe
+DELIMITER //
+
 CREATE TRIGGER IF NOT EXISTS update_calories_on_recipe_insert
 AFTER INSERT ON Recipe
 FOR EACH ROW
 BEGIN
-    CALL calculate_calories_per_portion(NEW.Recipe_name)
+    CALL calculate_calories_per_portion(NEW.name);
 END //
 
-DELIMITER ;
 
 CREATE TRIGGER IF NOT EXISTS update_calories_on_recipe_update
 AFTER UPDATE ON Recipe
 FOR EACH ROW
 BEGIN
-    CALL calculate_calories_per_portion(NEW.Recipe_name)
+    CALL calculate_calories_per_portion(NEW.name);
+END //
+
+
+CREATE TRIGGER IF NOT EXISTS update_calories_on_recipe_delete
+AFTER DELETE ON Recipe
+FOR EACH ROW
+BEGIN
+    DELETE FROM Nutrition_Info WHERE Recipe_name = OLD.name;
 END //
 
 DELIMITER ;
@@ -96,5 +126,9 @@ BEGIN
     SET NEW.age = YEAR(CURDATE()) - YEAR(NEW.date_of_birth) - 
                   (DATE_FORMAT(CURDATE(), '%m%d') < DATE_FORMAT(NEW.date_of_birth, '%m%d'));
 END //
+
+DROP PROCEDURE IF EXISTS `select_episode_participants`;
+DROP PROCEDURE IF EXISTS `check_participation_constraints`;
+DROP PROCEDURE IF EXISTS setup_episode;
 
 DELIMITER ;
